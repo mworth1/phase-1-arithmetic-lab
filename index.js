023880ba3da2e078a1b532781da1ff19bed48278
Miles Worthington <meworthington1@gmail.com>
2021-12-27 12:04:56 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply;
+let num1 = 31
+let num2 = 2
+multiply = num1 * num2
 
-// Write your code here
+let random;
+random = (Math.floor(Math.random()) + 1);
+
+let mod;
+let num3 = 20
+let num4 = 8;
+mod = num3 % num4;
+
+let max;
+max = Math.max(5, 10, 15, 20);
